777db362e5b3f8bd6ce3a9da16bfb993175abf4f
jayomeara <jay@idlewild.industries>
2022-07-10 21:40:47 -0600

    sunday assignement wrapup

---
+++ assignments/week1/nestedDictionariesAndLists/nestedDictionariesAndLists.py
@@ -0,0 +1,80 @@
+
+    # Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
+    # Change the last_name of the first student from 'Jordan' to 'Bryant'
+    # In the sports_directory, change 'Messi' to 'Andres'
+    # Change the value 20 in z to 30
+
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+x[1][0] = 15
+students[0]['last_name'] = 'Bryant'
+sports_directory['soccer'][0] = 'Andres'
+z[0]['y'] = 30
+
+
+# 2/3
+
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
+
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+def iterate_dictionary(list):
+    for i in range(0, len(list)):
+        for key,val in list[i].items():
+            print(f" {key} - {val},")
+
+iterate_dictionary(students)
+
+# 4
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+def print_info(dict):
+    for key,val in dict.items():
+        print("")
+        print(f"{len(val)} {key.upper()}")
+        for i in range(0,len(val)):
+            print(val[i])
+
+print_info(dojo)
+
+# # output:
+# 7 LOCATIONS
+# San Jose
+# Seattle
+# Dallas
+# Chicago
+# Tulsa
+# DC
+# Burbank
+    
+# 8 INSTRUCTORS
+# Michael
+# Amy
+# Eduardo
+# Josh
+# Graham
+# Patrick
+# Minh
+# Devon
--- notes/python_globalNotes.md
+++ notes/python_globalNotes.md
@@ -18,3 +18,25 @@ copy_of_words = words[:]
 #     y = y - 1
 # else:
 #     print("Final else statement")
+
+?? Exiting the function: Reaching a return statement always means "EXIT THIS FUNCTION NOW" whether or not there is more code. Any remaining code will not be run. ??
+
+my_dict = { "name": "Noelle", "language": "Python" }
+for each_key in my_dict:
+    print(my_dict[each_key])
+# output: Noelle, Python
+
+capitals = {"Washington":"Olympia","California":"Sacramento","Idaho":"Boise","Illinois":"Springfield","Texas":"Austin","Oklahoma":"Oklahoma City","Virginia":"Richmond"}
+# another way to iterate through the keys
+for key in capitals.keys():
+     print(key)
+# output: Washington, California, Idaho, Illinois, Texas, Oklahoma, Virginia
+#to iterate through the values
+for val in capitals.values():
+     print(val)
+# output: Olympia, Sacramento, Boise, Springfield, Austin, Oklahoma City, Richmond
+#to iterate through both keys and values
+for key, val in capitals.items():
+     print(key, " = ", val)
+# output: Washington = Olympia, California = Sacramento, Idaho = Boise, etc
+
