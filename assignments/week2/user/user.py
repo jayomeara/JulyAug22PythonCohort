bfa28cf8986bc84cba299cdb183f97526c0a7563
jayomeara <jay@idlewild.industries>
2022-07-12 10:25:58 -0600

    user.py assignment

--- assignments/week2/user/user.py
+++ assignments/week2/user/user.py
@@ -1,26 +1,46 @@
-# declare a class and give it name User
-class User:		
-    def __init__(self,first_name,last_name,age):
-        self.first_name = "Ada"
-        self.last_name = "Lovelace"
-        self.age = 42
+class User:
+    def __init__(self,first_name,last_name,email,age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.age = age
+        self.is_rewards_member = False
+        self.gold_card_points = 50
+    # Can't have default vales for required conditionals
+    def display_info(self):
+        print(self.first_name, self.last_name, self.email, self.age, self.is_rewards_member, self.gold_card_points)
 
-michael = User("Michael", "Clayton", 56)
-anna = User("Anna", "Wintor", 69)
+    def enroll(self):
+        self.is_rewards_member = self.is_rewards_member = True
 
-print(michael.age)
+    def spend_points(self,amount):
+        if self.gold_card_points <= amount:
+            print("Don't have enough points")
+        else: 
+            self.gold_card_points = self.gold_card_points - amount
 
-class Shoe:
-    # now our method has 4 parameters (including self)!
-    def __init__(self, brand, shoe_type, price):
-    	# we assign them accordingly
-        self.brand = brand
-        self.type = shoe_type
-        self.price = price
-        # the status is set to True by default
-        self.in_stock = True
+michael = User("Michael", "Clayton", "michael@gmail.com", 56)
+anna = User("Anna", "Wintor","anna@gmail.com", 69)
+joey = User("Joey", "Joey", "joey@gmail.com", 34)
+
+michael.display_info()
+michael.enroll()
+michael.spend_points(50)
+anna.spend_points(25)
+michael.display_info()
+anna.display_info()
+
+# class Shoe:
+#     # now our method has 4 parameters (including self)!
+#     def __init__(self, brand, shoe_type, price):
+#     	# we assign them accordingly
+#         self.brand = brand
+#         self.type = shoe_type
+#         self.price = price
+#         # the status is set to True by default
+#         self.in_stock = True
     
-    # Takes a float/percent as an argument and reduces the
-    # price of the item by that percentage.
-    def on_sale_by_percent(self, percent_off):
-        self.price = self.price * (1-percent_off)
+#     # Takes a float/percent as an argument and reduces the
+#     # price of the item by that percentage.
+#     def on_sale_by_percent(self, percent_off):
+#         self.price = self.price * (1-percent_off)
