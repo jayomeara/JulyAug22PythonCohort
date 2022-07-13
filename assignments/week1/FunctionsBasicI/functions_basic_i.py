aa1aa545d80679e131a886e8939dc45d7de50bcd
jayomeara <jay@idlewild.industries>
2022-07-13 10:14:53 -0600

    Wed morning updates

--- assignments/week1/FunctionsBasicI/functions_basic_i.py
+++ assignments/week1/FunctionsBasicI/functions_basic_i.py
@@ -1,47 +1,49 @@
 #1
-def number_of_food_groups():
-    return 5
-print(number_of_food_groups())
+# def number_of_food_groups():
+#     return 5
+# print(number_of_food_groups())
+# 5
 
 
 # #2
 # def number_of_military_branches():
 #     return 5
 # print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
-
+#not defined
 
 # #3
 # def number_of_books_on_hold():
 #     return 5
 #     return 10
 # print(number_of_books_on_hold())
-
+#10 <- 10 didn't overwrite 5
 
 # #4
 # def number_of_fingers():
 #     return 5
 #     print(10)
 # print(number_of_fingers())
-
+#10, 5 <- return ends the loop
 
 # #5
 # def number_of_great_lakes():
 #     print(5)
 # x = number_of_great_lakes()
 # print(x)
+# null <- none
 
 
 # #6
 # def add(b,c):
 #     print(b+c)
 # print(add(1,2) + add(2,3))
-
+# 8 <- printed 3 and 5
 
 # #7
 # def concatenate(b,c):
 #     return str(b)+str(c)
 # print(concatenate(2,5))
-
+# 2, 5
 
 # #8
 # def number_of_oceans_or_fingers_or_continents():
@@ -53,7 +55,7 @@ print(number_of_food_groups())
 #         return 10
 #     return 7
 # print(number_of_oceans_or_fingers_or_continents())
-
+# 100, 10
 
 # #9
 # def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
@@ -65,14 +67,14 @@ print(number_of_food_groups())
 # print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
 # print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
 # print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
-
+# 7, 14, 21
 
 # #10
 # def addition(b,c):
 #     return b+c
 #     return 10
 # print(addition(3,5))
-
+# 8
 
 # #11
 # b = 500
@@ -83,7 +85,7 @@ print(number_of_food_groups())
 # print(b)
 # foobar()
 # print(b)
-
+# 500, 300, 500, 300, 500 <- def isn't called until line 86
 
 # #12
 # b = 500
@@ -95,7 +97,7 @@ print(number_of_food_groups())
 # print(b)
 # foobar()
 # print(b)
-
+# 500, 500, 300, 300 <- why didn't b retain the 300 value?
 
 # #13
 # b = 500
@@ -107,7 +109,7 @@ print(number_of_food_groups())
 # print(b)
 # b=foobar()
 # print(b)
-
+# 500, 500, 300 , 300 <- I see why
 
 # #14
 # def foo():
@@ -117,16 +119,18 @@ print(number_of_food_groups())
 # def bar():
 #     print(3)
 # foo()
-
+# 1, 3, 2
 
 # #15
-# def foo():
-#     print(1)
-#     x = bar()
-#     print(x)
-#     return 10
-# def bar():
-#     print(3)
-#     return 5
-# y = foo()
-# print(y)
+def foo():
+    print(1)
+    x = bar()
+    print(x)
+    return 10
+def bar():
+    print(3)
+    return 5
+y = foo()
+print(y)
+# 1, x = 5, 
+# 10
