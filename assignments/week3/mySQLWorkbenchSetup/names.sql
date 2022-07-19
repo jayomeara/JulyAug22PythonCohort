e40877caec94e9083e26b0a7dc784cf482e73426
jayomeara <jay@idlewild.industries>
2022-07-19 10:15:21 -0600

    mySQL Workbench assignment

---
+++ assignments/week3/mySQLWorkbenchSetup/names.sql
@@ -0,0 +1,8 @@
+SELECT * FROM names;
+INSERT INTO names (name)
+VALUES('jay omeara');
+INSERT INTO names (name)
+VALUES ('joe schmo'), ('Mariano Rivera'), ('Frank Rivera');
+UPDATE names
+SET name = 'james omeara'
+WHERE id = 1;
