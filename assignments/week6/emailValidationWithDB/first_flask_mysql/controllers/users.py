e2da8d8019b87e6ba5c26dc3c8b39d7072863d29
jayomeara <jay@idlewild.industries>
2022-08-10 12:06:37 -0500

    0810 assignment push

--- assignments/week6/emailValidationWithDB/first_flask_mysql/controllers/users.py
+++
@@ -1,8 +0,0 @@
-from flask_app.models.user import User
-@app.route('/register', methods=['POST'])
-def register():
-    if not User.validate_user(request.form):
-        # we redirect to the template with the form.
-        return redirect('/')
-    # ... do other things
-    return redirect('/dashboard')
